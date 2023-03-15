9c83c08101ef690de44391066bbd137c9277380b
Miroslav Zeman <me@miroslavzeman.dev>
2023-03-15 17:24:35 +0100

    feat: add gpt-4 and other models support via --model option (#25)

--- packages/cli/src/lib/configuration.ts
+++ packages/cli/src/lib/configuration.ts
@@ -5,11 +5,17 @@ import * as fs from 'fs';
  * Persistent configuration interface
  */
 export interface Configuration {
-  apiKey?: string;
+  apiKey: string;
+  model: string;
   prompt?: string;
   usage?: boolean;
   messagesLimit?: number;
 }
+
+/**
+ * Default model which should be used if no model is provided
+ */
+export const DEFAULT_MODEL_CONFIG = 'gpt-3.5-turbo';
 
 const configPathname = path.join(__dirname, 'cli.configuration.json');
 
@@ -20,8 +26,11 @@ export const loadConfiguration = (): Configuration => {
   if (fs.existsSync(configPathname)) {
     // Load the configuration from the file
     const fileContent = fs.readFileSync(configPathname, 'utf8');
+    const configuration = JSON.parse(fileContent) as Configuration;
 
-    return JSON.parse(fileContent);
+    validateConfiguration(configuration);
+
+    return configuration;
   } else {
     console.error(
       'No configuration file found. You must initialize the tool with your API key before you start using it.'
@@ -36,13 +45,9 @@ export const loadConfiguration = (): Configuration => {
  * @param configuration
  */
 export const saveConfiguration = (configuration: Configuration) => {
-  if (!configuration.apiKey) {
-    console.error(
-      'No API key found. You must provide one with the --apiKey option before you start using the tool.'
-    );
+  configuration.model = configuration.model || DEFAULT_MODEL_CONFIG;
 
-    process.exit(1);
-  }
+  validateConfiguration(configuration);
 
   // Store the configuration in the file
   fs.writeFileSync(configPathname, JSON.stringify(configuration));
@@ -55,6 +60,30 @@ export const saveConfiguration = (configuration: Configuration) => {
 export const updateConfiguration = (configuration: Configuration) => {
   const updatedConfig = { ...loadConfiguration(), ...configuration };
 
+  validateConfiguration(updatedConfig);
+
   // Store the configuration in the file
   fs.writeFileSync(configPathname, JSON.stringify(updatedConfig));
+};
+
+/**
+ * Validate the configuration for API key and model
+ * @param configuration
+ */
+const validateConfiguration = (configuration: Configuration) => {
+  if (!configuration.apiKey) {
+    console.error(
+      'No API key found. You must provide one with the --apiKey option before you start using the tool.'
+    );
+
+    process.exit(1);
+  }
+
+  if (!configuration.model) {
+    console.error(
+      'No model found. You must provide one with the --model option before you start using the tool.'
+    );
+
+    process.exit(1);
+  }
 };
